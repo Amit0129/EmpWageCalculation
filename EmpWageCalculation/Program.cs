7ac833802ef87d5cd9406122ffd436761f5da8ad
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-22 19:43:10 +0530

    Added:UC5 Calculating wage for month taking 20 Working day

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -5,33 +5,35 @@
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAY = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Employee Wage Computation");
 
-            //Variable
-            int empHour = 0;
-            int empWage = 0;
-
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            //Switch Case
-
-            switch (empCheck)
+            int empHour = 0, empDalyWage = 0, totalMonthWage = 0;
+            //For loop
+            for (int i = 1; i <= NUM_OF_WORKING_DAY; i++)
             {
-                case IS_FULL_TIME:
-                    empHour = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHour = 4;
-                    break;
-                default:
-                    empHour = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHour = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHour = 4;
+                        break;
+                    default:
+                        empHour = 0;
+                        break;
+                }
+                empDalyWage = empHour * EMP_RATE_PER_HOUR;
+                totalMonthWage = totalMonthWage + empDalyWage;
+                Console.WriteLine($"Emp Wage Day {i} : {empDalyWage}");
             }
-            empWage = empHour * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employ Wage is : " + empWage);
+            Console.WriteLine("Total Wage Of The Month is: " + totalMonthWage);
         }
     }
 }
