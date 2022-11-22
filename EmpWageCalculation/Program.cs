a55e7fcf92c33cd278a8131d6f7937c25e1cbe5e
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-22 19:06:36 +0530

    Welcome message to Employee wage computation

---
+++ EmpWageCalculation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome To Employee Wage Computation");
+        }
+    }
+}
