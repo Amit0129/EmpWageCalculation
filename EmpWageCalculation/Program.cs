6f71c50501b579708e907d53cdf8b04b5e51a3c4
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-22 19:20:11 +0530

    Added:UC1 Employee Attendance

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Employee Wage Computation");
+
+            //Constant
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            //random = Reference Variable
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
