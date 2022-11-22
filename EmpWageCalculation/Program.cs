c2ac1d5a539ad49cb00f2ac02df986f82eddcb4e
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-22 19:33:31 +0530

    Added:UC4 Solving using Switch Case Statement in place of if else statement

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -2,13 +2,13 @@
 {
     internal class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Employee Wage Computation");
 
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_hOUR = 20;
             //Variable
             int empHour = 0;
             int empWage = 0;
@@ -16,22 +16,21 @@
             Random random = new Random();
             int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FULL_TIME)
+            //Switch Case
+
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Full Time");
-                empHour = 8;
+                case IS_FULL_TIME:
+                    empHour = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHour = 4;
+                    break;
+                default:
+                    empHour = 0;
+                    break;
             }
-            else if (empCheck == IS_PART_TIME)
-            {
-                Console.WriteLine("Employee is Part Time");
-                empHour = 4;
-            }
-            else
-            {
-                empHour = 0;
-                Console.WriteLine("Employ is Absent");
-            }
-            empWage = empHour * EMP_RATE_PER_hOUR;
+            empWage = empHour * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employ Wage is : " + empWage);
         }
     }
