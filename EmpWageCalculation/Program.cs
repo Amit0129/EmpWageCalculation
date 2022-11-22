5bc16d4095705785fbaed724acd3ecb54502a37c
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-22 19:28:37 +0530

    Added:UC3 Adding a part time employee

--- EmpWageCalculation/Program.cs
+++ EmpWageCalculation/Program.cs
@@ -6,26 +6,32 @@
         {
             Console.WriteLine("WelCome To Employee Wage Computation");
 
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_hOUR = 20;
             //Variable
             int empHour = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Full Time");
                 empHour = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Part Time");
+                empHour = 4;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
                 empHour = 0;
+                Console.WriteLine("Employ is Absent");
             }
-            empWage = empHour * EMP_RATE_PER_HOUR;
+            empWage = empHour * EMP_RATE_PER_hOUR;
             Console.WriteLine("Employ Wage is : " + empWage);
         }
     }
